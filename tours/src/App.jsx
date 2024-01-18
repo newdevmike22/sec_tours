184579f4abff2d12a164472b67bb75a6f047dfc0
MLDODSON <paola@dominicansoul.com>
2024-01-17 22:50:19 -0600

    Branch 2
    Branch 2 - Fetching the tour info from the api url.

--- tours/src/App.jsx
+++ tours/src/App.jsx
@@ -1,7 +1,30 @@
-const App = () => {
-  return (
-    <div>SEC Stadium Tours</div>
-  )
-};
+import { useEffect, useState } from "react";
+const url = "https://api.jsonbin.io/v3/b/65a8a9e4266cfc3fde7ac105";
 
-export default App;
+const App = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [tours, setTours] = useState([]);
+
+    const fetchTours = async () => {
+      setIsLoading(true)
+      try {
+        const response = await fetch(url);
+        const tours = await response.json();
+        console.log(tours);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    useEffect(() => {
+      fetchTours();
+    }, []);
+
+    return (
+      <div className="text-center">
+        SEC Stadium Tours
+      </div>
+    )
+  }
+  
+  export default App;
